aa58807e3fb4685777229036b9a783d112b0ee84
Александр Романов <sasha.sanya.romanov@mail.ru>
2022-12-18 19:33:50 +0300

    lists and quotes added

--- chapter2/chapter2-hierahical-structures.rkt
+++ chapter2/chapter2-hierahical-structures.rkt
@@ -30,3 +30,20 @@
 (list x1 y1)
 
 (pair? (list 1))
+
+(define (filter predicate sequence)
+  (cond ((null? sequence) null)
+        ((predicate (car sequence))
+         (cons (car sequence)
+               (filter predicate (cdr sequence))))
+        (else (filter predicate (cdr sequence)))))
+
+(filter odd? (list 1 2 3 4 5))
+
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
+
+(accumulate + 0 (list 1 2 3 4 5))
---
+++ chapter2/quotes.rkt
@@ -0,0 +1,8 @@
+#lang racket
+(define a 1)
+(define b 2)
+(list a b)
+(list 'a b)
+(list 'a 'b)
+(car '(a b c))
+(memq 'apple '(pear banana prune))
